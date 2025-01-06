ced93aaca0143073c8b0c8e77d0afc8204dc5c61
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2025-01-06 21:44:30 +0300

    feat: solving BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,140 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this.tree = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.tree;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+    if (this.tree === null) {
+      this.tree = newNode;
+    } else {
+      this._insertNode(this.tree, newNode);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this._insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this._insertNode(node.right, newNode);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this._hasNode(this.tree, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _hasNode(node, data) {
+    if (node.data === data) {
+      return true;
+    } else {
+      if (node.data > data) {
+        if (!node.left) return false;
+        return this._hasNode(node.left, data);
+      } else {
+        if (!node.right) return false;
+        return this._hasNode(node.right, data);
+      }
+    }
+  }
+
+  find(data) {
+    return this._findNode(this.tree, data);
+  }
+
+  _findNode(node, data) {
+    if (node.data === data) {
+      return node;
+    }
+
+    if (node.data > data) {
+      if (!node.left) return null;
+      return this._findNode(node.left, data);
+    } else {
+      if (!node.right) return null;
+      return this._findNode(node.right, data);
+    }
+  }
+
+  remove(data) {
+    this.tree = this._removeNode(this.tree, data);
+  }
+
+  _removeNode(node, data) {
+    if (node.data === null) {
+      return;
+    }
+
+    if (data < node.data) {
+      node.left = this._removeNode(node.left, data);
+      return node;
+    } else if (data > node.data) {
+      node.right = this._removeNode(node.right, data);
+      return node;
+    } else {
+      if (!node.right && !node.left) {
+        return null;
+      }
+
+      if (!node.right && node.left) {
+        return node.left;
+      }
+      if (!node.left && node.right) {
+        return node.right;
+      }
+
+      const minNode = this._findMin(node.right);
+      node.data = minNode;
+      node.right = this._removeNode(node.right, minNode);
+      return node;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._findMin(this.tree);
+  }
+
+  _findMin(node) {
+    if (node.left) {
+      return this._findMin(node.left);
+    } else {
+      return node.data;
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._findMax(this.tree);
+  }
+
+  _findMax(node) {
+    if (node.right) {
+      return this._findMax(node.right);
+    } else {
+      return node.data;
+    }
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
